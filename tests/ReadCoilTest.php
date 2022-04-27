ca0966ced014f2fe22c20d2fe3cce306ab336ea6
Elmatbaisaad <elmatbaisaad@gmail.com>
2022-04-27 16:25:35 +0200

    symfony supervision

---
+++ tests/ReadCoilTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Tests;
+use App\Repository\ReadCoil;
+use PHPUnit\Framework\TestCase;
+
+class ReadCoilTest extends TestCase
+{
+    protected $coil;
+
+    protected function setUp():void
+    {
+        $this->coil = new ReadCoil();
+        $this->coil->setIp('tcp://172.25.0.3:532');
+        $this->coil->setUnitId(0);
+    }
+
+    /** @test */
+    public function initialisation()
+    {
+        $this->assertEquals($this->coil->ip,'tcp://172.25.0.3:532');
+        $this->assertEquals($this->coil->unitId,0);
+    }
+
+    /** @test */
+    public function creating_connection()
+    {
+        $this->coil->setConnection(11,'my_coil');
+        $this->assertEquals($this->coil->message,'build succes');
+    }
+
+    /** @test */
+    public function receiving_the_right_value_of_coil()
+    {
+        $this->coil->setConnection(9,'my_coil');
+        $this->coil->getReponse();
+        $this->assertIsArray($this->coil->reponse_data,'pas array');
+        $this->assertEquals($this->coil->reponse_data['my_coil'],true);
+    }
+
+}
---
+++ tests/ReadRegistersTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Tests;
+use App\Repository\ReadRegister;
+use PHPUnit\Framework\TestCase;
+
+class ReadRegistersTest extends TestCase
+{
+    protected $conn;
+
+    protected function setUp():void
+    {
+        $this->conn = new ReadRegister();
+        $this->conn->setIp('tcp://172.25.0.3:532');
+        $this->conn->setUnitID(0);
+    }
+
+    /** @test */
+    public function creating_ip_and_unit_id()
+    {
+        $this->assertEquals($this->conn->ip,'tcp://172.25.0.3:532');
+        $this->assertEquals($this->conn->unitID,0);
+    }
+
+    /** @test */
+    public function creating_connection()
+    {
+        $this->conn->connection(2,'my_register');
+        $this->assertEquals($this->conn->message,'build succés');
+    }
+
+    /** @test */
+    public function receiving_the_right_value_of_register()
+    {
+        $this->conn->connection(2,'Sonde de Pression');
+        $this->conn->getResponse();
+        $this->assertArrayHasKey('Sonde de Pression',$this->conn->reponse_data);
+        $this->assertEquals($this->conn->reponse_data['Sonde de Pression'],216);
+    }
+
+}
